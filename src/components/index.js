04013449cd942e3f73ab7444afd69c0ce41ac29c
HAMMAD <hammad@HAMMADs-MBP.mshome.net>
2022-07-14 17:17:41 +0400

    header added

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,7 @@
-import NavigationDots from './NavigationDots';
-import SocialMedia from './SocialMedia';
+
 import Navbar from './Navbar/Navbar';
 
 export {
-    NavigationDots,
-    SocialMedia,
+
     Navbar,
 };
