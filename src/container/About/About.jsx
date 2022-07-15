b2bd48825489aab43c47a6110a51c8e90768feab
HAMMAD <hammad@HAMMADs-MBP.mshome.net>
2022-07-15 14:18:42 +0400

    about intigration with beckend

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+import { urlFor, client } from '../../client';
+
 
 import './About.scss';
+{/* 
+const abouts = [
+  { title: 'web Development', descrption: 'i am good web developer.', imgUrl: images.about01 },
+  { title: 'webdesign', descrption: 'i am good web developer.', imgUrl: images.about02 },
+  { title: 'App Development', descrption: 'i am good web developer.', imgUrl: images.about03 },
+  { title: 'Flutter Developer', descrption: 'i am good web developer.', imgUrl: images.about04 }
+];*/}
+
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default About
